24fa923c6f7902fbeeb954eb006299626b7d6f3e
tyler0611 <berlity0611@gmail.com>
2025-01-31 14:00:36 -0600

    Initial Commit

---
+++ Map Hackwich/Map_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwichApp.swift
+//  Map Hackwich
+//
+//  Created by Tyler Berlin on 1/31/25.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
