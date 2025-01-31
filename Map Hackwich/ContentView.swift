b16fa0d92a9d02f11114d590ca607148198da2d0
tyler0611 <berlity0611@gmail.com>
2025-01-31 14:02:52 -0600

    Setup initial Map

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -6,16 +6,24 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var region = MKCoordinateRegion(
+            center: CLLocationCoordinate2D(
+                latitude: 42.15704,
+                longitude: -88.14812),
+            span: MKCoordinateSpan(
+                latitudeDelta: 0.05,
+                longitudeDelta: 0.05)
+        )
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Map(coordinateRegion: $region)
         }
-        .padding()
     }
 }
 
